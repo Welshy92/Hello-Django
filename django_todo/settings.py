076c37470422af84bdfe9de91a64a7ad446af24e
Jacob Welsh <welshy92@outlook.com>
2023-03-19 18:05:55 +0000

    Change SKEY

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-#%73o25sen5c!t%th2_zts-o6nbd6bvd#sgebf-c1(g!m=b*6d")
+SECRET_KEY = os.environ.get("SECRET_KEY", "#%73o25sen5c!t%th2_zts-o6nbd6bvd#sgebf-c1(g!m=b*6d")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
